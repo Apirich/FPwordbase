4f6000d58fc2d209eb6084f13a3215d8af4b44ea
ApiRich <anhpikall@gmail.com>
2024-04-26 12:47:06 -0700

    Completed manually generate grid

--- wordGame/App.js
+++ wordGame/App.js
@@ -1,10 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Dimensions } from 'react-native';
+
+const screenDimensions = Dimensions.get("screen");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style = {styles.container}>
+      <View style = {styles.row}>
+        <View style = {styles.wordCell}>
+          <TextInput style = {styles.cell}
+                     value = "A"
+                     maxLength = {1}
+          />
+
+          <TextInput style = {styles.cell}
+                     value = "B"
+                     maxLength = {1}
+          />
+        </View>
+
+        <View style = {styles.wordCell}>
+          <TextInput style = {styles.cell}
+                     value = "C"
+                     maxLength = {1}
+          />
+
+          <TextInput style = {styles.cell}
+                     value = "D"
+                     maxLength = {1}
+          />
+        </View>
+
+        <View style = {styles.wordCell}>
+          <TextInput style = {styles.cell}
+                     value = "C"
+                     maxLength = {1}
+          />
+
+          <TextInput style = {styles.cell}
+                     value = "D"
+                     maxLength = {1}
+          />
+
+          <TextInput style = {styles.cell}
+                     value = "E"
+                     maxLength = {1}
+          />
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +57,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+
+  row: {
+    flexDirection: "row",
+  },
+
+  // wordCell: {
+  //   position: "relative",
+  // },
+
+  cell: {
+    borderWidth: 1,
+    borderColor: "green",
+    width: screenDimensions.width/10,
+    aspectRatio: 1,
+    textAlign: "center",
+  }
 });

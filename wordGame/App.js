f1b82d4663998c23097bd374bc5d1d8c6b66d6e4
ApiRich <anhpikall@gmail.com>
2024-04-28 14:45:00 -0700

    completed level and score

--- wordGame/App.js
+++ wordGame/App.js
@@ -3,19 +3,31 @@ import { StyleSheet, View, Text } from "react-native";
 
 import { wordsList, masterList } from "./words";
 import DisplayGame from "./grid";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+
+// const computeLevel = (score) => {
+//   const level = Math.ceil(score/15);
+//   return level;
+// };
 
 export default function App() {
   const crosswords = wordsList;
   const master = masterList;
 
-  const [disLevel, setDisLevel] = useState(1);
   const [disScore, setDisScore] = useState(0);
+  const [disLevel, setDisLevel] = useState(1);
+  // const [disLevel, setDisLevel] = useState(computeLevel(disScore));
+
+  useEffect(() => {
+    setDisLevel(1 + Math.floor(disScore/15));
+  }, [disScore]);
+
 
   const computeScore = (score) => {
     setDisScore(score);
   }
+
 
   return(
     <View style = {styles.container}>
@@ -24,7 +36,10 @@ export default function App() {
         <Text>Score: {disScore}</Text>
       </View>
 
-      <DisplayGame level = {disLevel} score = {disScore} computeScore = {computeScore} crosswordsProc = {crosswords} master = {master}/>
+      <DisplayGame level = {disLevel}
+                   score = {disScore} computeScore = {computeScore}
+                   crosswordsProc = {crosswords}
+                   master = {master}/>
       <StatusBar style="auto" />
     </View>
   );

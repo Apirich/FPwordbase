c8ef1c405f4d780fe769c0df2884760f0d90d5ca
ApiRich <anhpikall@gmail.com>
2024-05-03 19:05:36 -0700

    complete essential navigation

--- CrossWordleApp/App.js
+++ CrossWordleApp/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text, SafeAreaView, KeyboardAvoidingView, Platform } from "react-native";
+import { StyleSheet, View, Text, Dimensions, SafeAreaView, KeyboardAvoidingView, Platform, Image, ActivityIndicator, TouchableOpacity } from "react-native";
 import { useEffect, useState } from "react";
 
 import DisplayGame from "./src/components/grid";
@@ -10,8 +10,12 @@ import { generate, count } from "random-words";
 // Import navigation
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator} from "@react-navigation/stack";
+// Import NetInfo (NetInfo has event listenter)
+import NetInfo from '@react-native-community/netinfo';
 
+const screenDimensions = Dimensions.get("screen");
 
+// -------- Generate words positions --------
 const randomPick = (itemList, loopTime, libName) => {
   while(itemList.length < loopTime){
     chosenItem = libName[Math.floor(Math.random() * libName.length)]
@@ -23,6 +27,8 @@ const randomPick = (itemList, loopTime, libName) => {
   return itemList;
 };
 
+
+// -------- Generate grids data (game) --------
 const generateLibrary = (gen, level, game, word) => {
   const positions = [0, 1, 2, 3, 4];
   let wXpos = [];
@@ -58,10 +64,8 @@ const generateLibrary = (gen, level, game, word) => {
 };
 
 
-// -------- Navigator --------
-const Stack = createStackNavigator();
-
-export default function App() {
+// -------- Game Screen --------
+OfflineGameScreen = ({navigation, route}) => {
   const maxLevel = 10;
   const gamePerLevel = 3;
 
@@ -141,6 +145,10 @@ export default function App() {
   return(
     <KeyboardAvoidingView style = {styles.container} behavior = {Platform.OS === "ios" ? "padding" : "height"}>
       <SafeAreaView style = {styles.safeArea}>
+        <TouchableOpacity style = {styles.button} onPress = {() => navigation.goBack()}>
+          <Text style = {[styles.buttonText]}>Home</Text>
+        </TouchableOpacity>
+
         <View style = {styles.scoreLvlContainer}>
           <Text>Level: {disLevel}</Text>
           <Text>Score: {disScore}</Text>
@@ -160,8 +168,132 @@ export default function App() {
       </SafeAreaView>
     </KeyboardAvoidingView>
   );
-}
+};
 
+
+// -------- Login Screen --------
+LoginScreen = ({navigation, route}) => {
+  return(
+    <View style = {styles.container}>
+      <SafeAreaView>
+        <Text>Please Sign up or Login</Text>
+
+        <TouchableOpacity style = {styles.button}>
+          <Text style = {[styles.buttonText]}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style = {styles.button}>
+          <Text style = {[styles.buttonText]}>Sign Up</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style = {styles.button} onPress = {() => navigation.goBack()}>
+          <Text style = {[styles.buttonText]}>Home</Text>
+        </TouchableOpacity>
+    </SafeAreaView>
+    </View>
+  );
+};
+
+
+// -------- Welcome Screen --------
+WelcomeScreen = ({navigation, route}) => {
+  const { internetChecking } = route.params;
+
+  return(
+    <View style = {styles.container}>
+      <SafeAreaView>
+        <Text>Welcom to The Crosswordle App</Text>
+        <Text>Please Choose Gaming Mode</Text>
+
+        <TouchableOpacity style = {styles.button} onPress = {() => navigation.navigate("Login")} disabled = {internetChecking ? false : true}>
+          <Text style = {[styles.buttonText, internetChecking ? null : styles.disableButtonText]}>Online Mode</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style = {styles.button} onPress = {() => navigation.navigate("OfflineGame")}>
+          <Text style = {[styles.buttonText]}>Offline Mode</Text>
+        </TouchableOpacity>
+    </SafeAreaView>
+    </View>
+  );
+};
+
+
+// -------- Splash Screen --------
+SplashScreen = (internetStatus) => {
+  return(
+    <View style = {styles.container}>
+      <SafeAreaView>
+        <Image
+            style = {styles.logo}
+            source = {require("./assets/crosswordleLogo.png")}
+        />
+
+        <ActivityIndicator size = {"large"}/>
+
+        <Text>{internetStatus ? "" : "Your internet connection status is offline, only Offline Mode will be available!"}</Text>
+    </SafeAreaView>
+    </View>
+  );
+};
+
+
+// -------- Navigator --------
+const Stack = createStackNavigator();
+
+export default function App(){
+  const [loading, setLoading] = useState(true);
+  const [internetChecking, setInternetChecking] = useState(true);
+
+  const processLoading = async () => {
+    // Splashing for 3 seconds
+    const sleep = splashSecond => new Promise(anyFunctionIfHave => setTimeout(anyFunctionIfHave, splashSecond));
+
+    try{
+      await sleep(3000);
+    }
+    finally{
+      setLoading(false);
+    }
+  }
+
+  // Using NetInfo to check internet connection
+  const unsubscribe = NetInfo.addEventListener(state => {
+    if(internetChecking != state.isConnected){
+      setInternetChecking(state.isConnected);
+    }
+  });
+
+  useEffect(() => {
+    processLoading();
+  }, []);
+
+
+  if(loading){
+    return SplashScreen(internetChecking);
+  }
+
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName = "Welcome">
+        <Stack.Screen name = "Welcome" component = {WelcomeScreen}
+                      options = {{headerShown: false}}
+                      initialParams = {{internetChecking}}
+        />
+
+        <Stack.Screen name = "Login" component = {LoginScreen}
+                      options = {{headerShown: false}}
+        />
+
+        <Stack.Screen name = "OfflineGame" component = {OfflineGameScreen}
+                      options = {{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+
+// -------- Styles --------
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -170,6 +302,26 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 
+  // Splash Screen
+  logo: {
+    width: 120,
+    height: 120,
+    alignSelf: "center",
+    marginBottom: 20,
+  },
+
+  // Welcome Screen
+  buttonText: {
+    color: "green",
+    fontSize: screenDimensions.width/20,
+  },
+
+  disableButtonText: {
+    color: "grey",
+    fontSize: screenDimensions.width/20,
+  },
+
+  // Game Screen
   safeArea: {
     flex: 1,
   },
